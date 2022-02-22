3d702c9ce5f5f85218f3051e7fd8ff48e724da7a
MqyGalaxy <2412083655@qq.com>
2022-02-23 06:03:30 +0800

    Site updated: 2022-02-23 06:03:29

--- js/lunbo.js
+++ js/lunbo.js
@@ -1,3 +1,3 @@
-// build time:Mon Feb 21 2022 06:06:18 GMT+0800 (中国标准时间)
+// build time:Wed Feb 23 2022 06:03:25 GMT+0800 (中国标准时间)
 function i(i){$(i).find("ul:first").animate({marginTop:"-25px"},500,function(){$(this).css({marginTop:"0px"}).find("li:first").appendTo(this)})}$(document).ready(function(){li_num=$("#myscroll ul li").length;tenki_speed=4e3;if(li_num>1){setInterval('AutoScroll("#myscroll")',tenki_speed)}});
 //rebuild by hrmmi 
