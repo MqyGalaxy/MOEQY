87fbae2e0a3215c49630c6fe50409c362efd16da
MqyGalaxy <2412083655@qq.com>
2022-04-03 01:14:17 +0800

    Site updated: 2022-04-03 01:14:16

--- js/wow_init.js
+++ js/wow_init.js
@@ -1,3 +1,3 @@
-// build time:Fri Apr 01 2022 09:41:27 GMT+0800 (中国标准时间)
+// build time:Sun Apr 03 2022 01:14:12 GMT+0800 (中国标准时间)
 wow=new WOW({boxClass:"wow",animateClass:"animate__animated",offset:0,mobile:false,live:true});wow.init();
 //rebuild by hrmmi 
