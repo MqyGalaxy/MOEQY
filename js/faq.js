d5f84a464cc4d34c12aed7c9f19ff445027469bf
MqyGalaxy <2412083655@qq.com>
2023-07-29 11:36:29 +0800

    Site updated: 2023-07-29 11:36:25

--- js/faq.js
+++ js/faq.js
@@ -1,3 +1,3 @@
-// build time:Wed Jul 26 2023 05:03:30 GMT+0800 (中国标准时间)
+// build time:Sat Jul 29 2023 11:36:06 GMT+0800 (中国标准时间)
 var e=document.getElementsByClassName("accordion");var t;for(t=0;t<e.length;t++){e[t].onclick=function(){this.classList.toggle("active");var e=this.nextElementSibling;if(e.style.maxHeight){e.style.maxHeight=null}else{e.style.maxHeight=e.scrollHeight+"px"}}}
 //rebuild by hrmmi 
--- js/loading.js
+++ js/loading.js
@@ -1,3 +1,3 @@
-// build time:Wed Jul 26 2023 05:03:30 GMT+0800 (中国标准时间)
+// build time:Sat Jul 29 2023 11:36:06 GMT+0800 (中国标准时间)
 window.onload=function(){setTimeout(function(){var e=document.getElementsByClassName("loading")[0];e.className="loading fadeout";setTimeout(function(){e.style.display="none"},1e3)},1e3)};
 //rebuild by hrmmi 
