346eedb689ea7f291981e2b9b12c4aafe8db530b
MqyGalaxy <2412083655@qq.com>
2023-08-08 13:22:58 +0800

    Site updated: 2023-08-08 13:22:55

--- js/chooseLang.js
+++ js/chooseLang.js
@@ -1,3 +1,3 @@
-// build time:Tue Aug 08 2023 13:02:50 GMT+0800 (中国标准时间)
+// build time:Tue Aug 08 2023 13:22:30 GMT+0800 (中国标准时间)
 document.addEventListener("DOMContentLoaded",function(){var e=0;document.getElementById("langFooter").onclick=function(){if(e===0){document.getElementById("langFooterItem").style.visibility="visible";document.getElementById("langFooterItem").style.opacity="1";document.getElementById("langFooter").style.color="#fff";document.getElementById("langFooterIcon").style.transform="rotate(180deg)";e=1}else{document.getElementById("langFooterItem").style.visibility="";document.getElementById("langFooterItem").style.opacity="";document.getElementById("langFooter").style.color="";document.getElementById("langFooterIcon").style.transform="";e=0}}});
 //rebuild by hrmmi 
--- js/window.js
+++ js/window.js
@@ -1,3 +1,3 @@
-// build time:Tue Aug 08 2023 13:02:50 GMT+0800 (中国标准时间)
+// build time:Tue Aug 08 2023 13:22:30 GMT+0800 (中国标准时间)
 var e=document.getElementById("vdo-botton");var n=document.getElementById("vdo-home");e.onclick=function o(){n.style.display="flex"};n.onclick=function t(){n.style.display="none"};
 //rebuild by hrmmi 
