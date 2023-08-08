3e934d2379c8754e9d07738ce4fc31d432c71a76
MqyGalaxy <2412083655@qq.com>
2023-08-08 12:52:34 +0800

    Site updated: 2023-08-08 12:52:31

--- js/wow_init.js
+++ js/wow_init.js
@@ -1,3 +1,3 @@
-// build time:Tue Aug 08 2023 12:47:58 GMT+0800 (中国标准时间)
+// build time:Tue Aug 08 2023 12:51:51 GMT+0800 (中国标准时间)
 wow=new WOW({boxClass:"wow",animateClass:"animate__animated",offset:0,mobile:false,live:true});wow.init();
 //rebuild by hrmmi 
