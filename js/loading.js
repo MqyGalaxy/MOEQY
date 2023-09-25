381dbe41fa3b8d35c829a2e14aec2eac315d0683
MqyGalaxy <2412083655@qq.com>
2023-09-26 01:51:18 +0800

    Site updated: 2023-09-26 01:51:15

--- js/loading.js
+++ js/loading.js
@@ -1,3 +1,3 @@
-// build time:Sat Sep 23 2023 10:13:52 GMT+0800 (中国标准时间)
+// build time:Tue Sep 26 2023 01:50:40 GMT+0800 (中国标准时间)
 window.onload=function(){setTimeout(function(){var e=document.getElementsByClassName("loading")[0];e.className="loading fadeout";setTimeout(function(){e.style.display="none"},1e3)},1e3)};
 //rebuild by hrmmi 
