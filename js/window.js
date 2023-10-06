83d6f932b40ef63a9ea4088284e27f4660a25b5f
MqyGalaxy <2412083655@qq.com>
2023-10-06 12:20:53 +0800

    Site updated: 2023-10-06 12:20:49

--- js/window.js
+++ js/window.js
@@ -1,3 +1,3 @@
-// build time:Tue Sep 26 2023 01:50:40 GMT+0800 (中国标准时间)
+// build time:Fri Oct 06 2023 12:20:15 GMT+0800 (中国标准时间)
 var e=document.getElementById("vdo-botton");var n=document.getElementById("vdo-home");e.onclick=function o(){n.style.display="flex"};n.onclick=function t(){n.style.display="none"};
 //rebuild by hrmmi 
--- js/wow_init.js
+++ js/wow_init.js
@@ -1,3 +1,3 @@
-// build time:Tue Sep 26 2023 01:50:40 GMT+0800 (中国标准时间)
+// build time:Fri Oct 06 2023 12:20:15 GMT+0800 (中国标准时间)
 wow=new WOW({boxClass:"wow",animateClass:"animate__animated",offset:0,mobile:false,live:true});wow.init();
 //rebuild by hrmmi 
