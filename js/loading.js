b79e8a03d9bb894a791ff8e0f09e05163788aa7b
MqyGalaxy <2412083655@qq.com>
2023-10-29 03:01:41 +0800

    Site updated: 2023-10-29 03:01:37

--- js/loading.js
+++ js/loading.js
@@ -1,3 +1,3 @@
-// build time:Fri Oct 06 2023 12:20:15 GMT+0800 (中国标准时间)
+// build time:Sun Oct 29 2023 03:00:59 GMT+0800 (中国标准时间)
 window.onload=function(){setTimeout(function(){var e=document.getElementsByClassName("loading")[0];e.className="loading fadeout";setTimeout(function(){e.style.display="none"},1e3)},1e3)};
 //rebuild by hrmmi 
--- js/wow_init.js
+++ js/wow_init.js
@@ -1,3 +1,3 @@
-// build time:Fri Oct 06 2023 12:20:15 GMT+0800 (中国标准时间)
+// build time:Sun Oct 29 2023 03:00:59 GMT+0800 (中国标准时间)
 wow=new WOW({boxClass:"wow",animateClass:"animate__animated",offset:0,mobile:false,live:true});wow.init();
 //rebuild by hrmmi 
